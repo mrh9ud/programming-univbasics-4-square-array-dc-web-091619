60f6b18889ee225ac3db5f18b37e830a9744b5b8
Matthew Heavner <mrh9ud@virginia.edu>
2019-09-05 15:32:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
  
   while array[counter] do
     new_array = []
-    new_array << array[counter] ** 2
+    new_array.push array[counter] ** 2
     counter += 1 
     print new_array
   end
